e0feeda95955c776773a334d99fc18a5efeec362
Sean Duffy <kqrpnb@gmail.com>
2022-07-28 20:52:17 -0400

    Updated gradle as per instructions. Implemented printJobs().

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -120,6 +120,23 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("printJobs is not implemented yet");
+//        System.out.println(someJobs);
+        if (someJobs.isEmpty()) {
+            System.out.print("No Results");
+            return;
+        }
+        for (HashMap<String, String> listing : someJobs) {
+            System.out.println("\n*****");
+
+            for (Map.Entry<String, String> row : listing.entrySet()) {
+                System.out.print(row.getKey());
+                System.out.print(": ");
+                System.out.print(row.getValue());
+                System.out.print("\n");
+            }
+
+            System.out.println("*****");
+        }
+//        System.out.println("printJobs is not implemented yet");
     }
 }
